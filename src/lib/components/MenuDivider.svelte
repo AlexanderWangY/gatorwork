f9f9a4aa69ecb73bfc5e309ec994647adb9d90ea
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-04-06 18:34:24 -0400

    finished navbar

---
+++ src/lib/components/MenuDivider.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="text-brand-gray flex flex-col gap-0.5 text-sm">
+	<p>{title}</p>
+	<div class="w-full border-[1px] border-[#C4C4C4]"></div>
+</div>
